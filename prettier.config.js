a0f2451d338caa0d28ae7227396d9b596837d956
Sadraque Santos <sdq.sts@gmail.com>
2024-10-23 03:29:23 -0300

    Update code formatting configuration

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  semi: true,
+  trailingComma: 'es5',
+  singleQuote: true,
+  printWidth: 120,
+  htmlWhitespaceSensitivity: 'ignore',
+  vueIndentScriptAndStyle: false,
+  tabWidth: 2,
+};
